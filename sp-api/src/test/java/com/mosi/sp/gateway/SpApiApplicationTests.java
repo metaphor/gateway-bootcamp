9632319746c5e13a4a3c7ca30a1c536b5eb7da8a
Zhiheng Li <zhiheng.li@mosi-tech.com>
2020-11-11 18:11:17 +0800

    init api,auth,gateway

---
+++ sp-api/src/test/java/com/mosi/sp/gateway/SpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mosi.sp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ sp-auth/src/main/java/com/mosi/sp/gateway/SpAuthApplication.java
@@ -0,0 +1,13 @@
+package com.mosi.sp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpAuthApplication.class, args);
+	}
+
+}
